dc1ed981ee393ab7388e0a6ae2aaea60e69bc3b5
Thien Tan <nttansv@gmail.com>
2024-05-04 09:16:52 +0700

    New/Minio & Ubuntu

---
+++ Postgres/data/data.sql
@@ -0,0 +1,19 @@
+--
+-- PostgreSQL database cluster dump
+--
+
+SET default_transaction_read_only = off;
+
+SET client_encoding = 'UTF8';
+SET standard_conforming_strings = on;
+
+--
+-- Databases
+--
+
+--
+-- Database "template1" dump
+--
+
+\connect template1
+
